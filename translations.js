693660b27992f8dcbc00ac24c2c1eaec70a52cfe
Tobias Eriksson <toberi@outlook.com>
2024-04-09 03:40:53 +0200

    cleanup, refactor, new design

---
+++ translations.js
@@ -0,0 +1,232 @@
+const translations = {
+  se: {
+    1: "Systemutvecklare | DevOps",
+    2: `Säker infrastruktur &bull;
+    Databasmigrering &bull;
+    Generativ AI`,
+    3: "Erfarenhet",
+    4: "AFRY, Systemutvecklare.",
+    5: "Heltid, Aug 2022 - Mar 2023",
+    6: "Webbutveckling fullstack i ProTAK, en programvara för produktionsoptimering.",
+    7: `C# &bull;
+    .NET &bull;
+    Typescript &bull;
+    React &bull;
+    Knockout &bull;
+    Jquery &bull;
+    Entity Framework &bull;
+    PLSQL &bull;
+    D3 &bull;
+    HTML &bull;
+    SCSS &bull;
+    Azure DevOps &bull;
+    Agila metoder`,
+    8: "Flexeurope, Teknikinformatör.",
+    9: "Heltid, Feb 2021 - Jul 2021",
+    10: "Agilt arbete i internationellt team med författande av manualer för webb/klientprogramvara, APIer & integrationer.",
+    11: "Manuell testning av programvara och test av funktionalitet för dokumentation.",
+    12: "Installationer & konfiguration av programvara och tillhörande system, databaser m.m.",
+    13: `Teknisk dokumentation &bull;
+    Funktionsanalys &bull;
+    Funktionstester &bull;
+    Systemkonfiguration &bull;
+    Agila metoder &bull;
+    Jira`,
+    14: "CoreIT, Systemutvecklare.",
+    15: "Heltid, Apr 2018 - Jun 2018, Nov 2018 - Apr 2019",
+    16: "Utveckling av webbapplikation för kunder att prenumerera på driftinformation inom utbud av företagstjänster.",
+    17: "Utveckling av windows applikation för hantering av projekt och affärsdata.",
+    18: "Projekt, NetViz.",
+    19: "Nov 2023 - Pågående",
+    20: "Webbaserad nätverksskanner integrerad med Nmap.",
+    21: "Skanna och visualisera nätverk och visa detaljer om anslutna enheter.",
+    22: `React &bull;
+    Typescript &bull;
+    .NET &bull;
+    C# &bull;
+    Nmap &bull;
+    D3 &bull;
+    Redux &bull;
+    Material UI &bull;
+    Vite &bull;
+    Datavisualisering &bull;
+    Nätverksteknik &bull;
+    Cybersäkerhet`,
+    23: "Projekt, Vision.",
+    24: "Jul 2023 - Pågående",
+    25: "Datadriven och användarvänlig SaaS applikation byggd på Azure plattformen för att visualisera data på olika vis och med stöd av ML skapa dataprognoser. Demo finns tillgänglig på länk nedan, meddela om intresse.",
+    26: `Öppna i ny flik <i class="fas fa-external-link-alt"></i>`,
+    27: "Licenser och certifikat",
+    28: "Utfärdad feb. 2024",
+    29: "Utfärdad feb. 2024",
+    30: "Utfärdad jan. 2024",
+    31: "Utfärdad jan. 2024",
+    32: "Utfärdad jan. 2024",
+    33: "Utfärdad dec. 2023",
+    34: "Kompetenser",
+    35: "Språk & ramverk",
+    36: "Tekniker",
+    37: "Övriga",
+    38: `Webbutveckling &bull;
+    Frontend &bull;
+    Backend &bull;
+    Mjukvaruutveckling &bull;
+    Generativ AI &bull;
+    Automation &bull;
+    Dataanalys &bull;
+    Datavisualisering &bull;
+    Identitets- och åtkomsthantering (IAM) &bull;
+    Datamigrering &bull;
+    Agila metoder &bull;
+    Funktionsanalys &bull;
+    Funktionstester &bull;
+    Enhetstester &bull;
+    Teknisk dokumentation &bull;
+    Systemkonfiguration &bull;
+    Cybersäkerhet &bull;
+    Nätverksinfrastruktur`,
+    39: "Utbildning",
+    40: "Yrkeshögskoleexamen",
+    41: "Systemutvecklare .NET",
+    42: "Kurser",
+    43: "Nätverkssäkerhet",
+    44: "Nätverksteknik",
+    45: "Nätverksteknologier",
+    46: "Nätverksadministration",
+    47: "Administration av nätverks- och serverutrustning",
+    48: "Övrigt",
+    49: "Referenser lämnas på begäran.",
+    50: "Körkort B",
+    51: "Engelska, professionell nivå.",
+    link: `Visa legitimering <i class="fas fa-external-link-alt"></i>`,
+  },
+  en: {
+    1: "Software Developer | DevOps",
+    2: `Secure infrastructure &bull;
+    Database migration &bull;
+    Generative AI`,
+    3: "Experience",
+    4: "AFRY, Software Developer.",
+    5: "Full time, Aug 2022 - Mar 2023",
+    6: "Web development full stack in ProTAK, a software for production optimization.",
+    7: `C# &bull;
+    .NET &bull;
+    Typescript &bull;
+    React &bull;
+    Knockout &bull;
+    Jquery &bull;
+    Entity Framework &bull;
+    PLSQL &bull;
+    D3 &bull;
+    HTML &bull;
+    SCSS &bull;
+    Azure DevOps &bull;
+    Agile methods`,
+    8: "Flexeurope, Technical Writer.",
+    9: "Full time, Feb 2021 - Jul 2021",
+    10: "Agile work in international team with writing of manuals for web/client software, APIs & integrations.",
+    11: "Manual testing of software and testing of functionality for documentation.",
+    12: "Installations & configuration of software and associated systems, databases, etc.",
+    13: `Technical documentation &bull;
+    Functional analysis &bull;
+    Functional testing &bull;
+    System configuration &bull;
+    Agile methods &bull;
+    Jira`,
+    14: "CoreIT, Software developer.",
+    15: "Full time, Apr 2018 - Jun 2018, Nov 2018 - Apr 2019",
+    16: "Development of web application for customers to subscribe to receive operational information about company services.",
+    17: "Development of windows application for managing projects and business data.",
+    18: "Project, NetViz.",
+    19: "Nov 2023 - Ongoing",
+    20: "Web based network scanner integrated with Nmap.",
+    21: "Scan and visualize networks and view details about connected devices.",
+    22: `React &bull;
+    Typescript &bull;
+    .NET &bull;
+    C# &bull;
+    Nmap &bull;
+    D3 &bull;
+    Redux &bull;
+    Material UI &bull;
+    Vite &bull;
+    Data visualization &bull;
+    Network technology &bull;
+    Cybersecurity`,
+    23: "Project, Vision.",
+    24: "Jul 2023 - Ongoing",
+    25: "Data-driven and user-friendly SaaS application built on the Azure platform to visualize data in different ways and create data forecasts with the support of ML. Demo is available at the link below, let me know if you are interested.",
+    26: `Open in new tab <i class="fas fa-external-link-alt"></i>`,
+    27: "Licenses and certificates",
+    28: "Issued feb. 2024",
+    29: "Issued feb. 2024",
+    30: "Issued jan. 2024",
+    31: "Issued jan. 2024",
+    32: "Issued jan. 2024",
+    33: "Issued dec. 2023",
+    34: "Skills",
+    35: "Languages & frameworks",
+    36: "Technologies",
+    37: "Other",
+    38: `Web development &bull;
+    Frontend &bull;
+    Backend &bull;
+    Software development &bull;
+    Generative AI &bull;
+    Automation &bull;
+    Data analysis &bull;
+    Data visualization &bull;
+    Identity and Access Management (IAM) &bull;
+    Database migration &bull;
+    Agile methods &bull;
+    Functional analysis &bull;
+    Functional testing &bull;
+    Unit testing &bull;
+    Technical documentation &bull;
+    System configuration &bull;
+    Cybersecurity &bull;
+    Network infrastructure`,
+    39: "Education",
+    40: "HVE Diploma",
+    41: "Software Developer .NET",
+    42: "Courses",
+    43: "Network security",
+    44: "Network technology",
+    45: "Network technologies",
+    46: "Network administration",
+    47: "Administration of network and server equipment",
+    48: "Other",
+    49: "References available upon request.",
+    50: "Drivers license",
+    51: "Swedish, native.",
+    link: `Show identification <i class="fas fa-external-link-alt"></i>`,
+  },
+};
+
+let currentLang = "se";
+
+const btn = document.getElementById("switchLangBtn");
+
+btn.onclick = function (e) {
+  e.preventDefault();
+  currentLang = currentLang === "se" ? "en" : "se";
+  document.querySelector("html").lang = currentLang;
+  translatePage(currentLang);
+  btn.title = currentLang === "se" ? "View in English" : "Visa på Svenska";
+  if (!isDarkTheme) {
+    currentLang === "se"
+      ? (themeBtn.title = "Växla till mörkt tema")
+      : (themeBtn.title = "Switch to dark theme");
+  } else {
+    currentLang === "se"
+      ? (themeBtn.title = "Växla till ljust tema")
+      : (themeBtn.title = "Switch to light theme");
+  }
+};
+
+function translatePage(lang) {
+  document.querySelectorAll("[data-t]").forEach((el) => {
+    const key = el.getAttribute("data-t");
+    el.innerHTML = translations[lang][key];
+  });
+}
