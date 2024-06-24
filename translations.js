a0b55377eb75db21edabf5d00ae578331599bddb
Tobias Eriksson <toberi@outlook.com>
2024-06-24 13:48:22 +0200

    style/layout rework

--- translations.js
+++ translations.js
@@ -1,201 +1,105 @@
 const translations = {
   se: {
-    1: "Systemutvecklare | DevOps",
-    2: `Säker infrastruktur &bull;
-    Databasmigrering &bull;
-    Generativ AI`,
-    3: "Erfarenhet",
-    4: "AFRY, Systemutvecklare.",
-    5: "Heltid, Aug 2022 - Mar 2023",
-    6: "Webbutveckling fullstack i ProTAK, en programvara för produktionsoptimering.",
-    7: `C# &bull;
-    .NET &bull;
-    Typescript &bull;
-    React &bull;
-    Knockout &bull;
-    Jquery &bull;
-    Entity Framework &bull;
-    PLSQL &bull;
-    D3 &bull;
-    HTML &bull;
-    SCSS &bull;
-    Azure DevOps &bull;
-    Agila metoder`,
-    8: "Flexeurope, Teknikinformatör.",
-    9: "Heltid, Feb 2021 - Jul 2021",
-    10: "Agilt arbete i internationellt team med författande av manualer för webb/klientprogramvara, APIer & integrationer.",
-    11: "Manuell testning av programvara och test av funktionalitet för dokumentation.",
-    12: "Installationer & konfiguration av programvara och tillhörande system, databaser m.m.",
-    13: `Teknisk dokumentation &bull;
-    Funktionsanalys &bull;
-    Funktionstester &bull;
-    Systemkonfiguration &bull;
-    Agila metoder &bull;
-    Jira`,
-    14: "CoreIT, Systemutvecklare.",
-    15: "Heltid, Apr 2018 - Jun 2018, Nov 2018 - Apr 2019",
-    16: "Utveckling av webbapplikation för kunder att prenumerera på driftinformation inom utbud av företagstjänster.",
-    17: "Utveckling av windows applikation för hantering av projekt och affärsdata.",
-    18: "Projekt, NetViz.",
-    20: "Webbaserad nätverksskanner integrerad med Nmap.",
-    21: "Skanna och visualisera nätverk och visa detaljer om anslutna enheter.",
-    22: `React &bull;
-    Typescript &bull;
-    .NET &bull;
-    C# &bull;
-    Nmap &bull;
-    D3 &bull;
-    Redux &bull;
-    Material UI &bull;
-    Vite &bull;
-    Datavisualisering &bull;
-    Nätverksteknik &bull;
-    Cybersäkerhet`,
-    23: "Projekt, Vision.",
-    25: "Datadriven och användarvänlig SaaS applikation byggd på Azure plattformen för att visualisera data på olika vis och med stöd av ML skapa dataprognoser. Demo finns tillgänglig på länk nedan, meddela om intresse.",
-    26: `Öppna i ny flik <i class="fas fa-external-link-alt"></i>`,
-    27: "Licenser och certifikat",
-    28: "Utfärdad feb. 2024",
-    29: "Utfärdad feb. 2024",
-    30: "Utfärdad jan. 2024",
-    31: "Utfärdad jan. 2024",
-    32: "Utfärdad jan. 2024",
-    33: "Utfärdad dec. 2023",
-    34: "Kompetenser",
-    35: "Språk & ramverk",
-    36: "Tekniker",
-    37: "Övriga",
-    38: `Webbutveckling &bull;
-    Frontend &bull;
-    Backend &bull;
-    Mjukvaruutveckling &bull;
-    Generativ AI &bull;
-    Automation &bull;
-    Dataanalys &bull;
-    Datavisualisering &bull;
-    Identitets- och åtkomsthantering (IAM) &bull;
-    Datamigrering &bull;
-    Agila metoder &bull;
-    Funktionsanalys &bull;
-    Funktionstester &bull;
-    Enhetstester &bull;
-    Teknisk dokumentation &bull;
-    Systemkonfiguration &bull;
-    Cybersäkerhet &bull;
+    loc: `<i class="fas fa-map-marker-alt"></i> Örnsköldsvik, Sverige`,
+    a_h2: "Erfarenhet",
+    a_h4_1: "AFRY, Systemutvecklare.",
+    a_ul_1_li_1:
+      "Webbutveckling i ProTAK, en programvara för produktionsoptimering.",
+    a_ul_1_li_2:
+      "Utvecklade BI rapport lösning åt kund för att förenkla redovisning av produktionsdata.",
+    a_ul_1_li_3:
+      "Arbete med modernisering av komponenter från KnockoutJS till ReactJS och diverse supportärenden.",
+    a_h4_2: "Flexeurope, Teknikinformatör",
+    a_ul_2_li_1:
+      "Slutförde arbete med författande av programvarumanualer för äldre och fram till aktuella versioner.",
+    a_ul_2_li_2:
+      "Systemkonfiguration och test av funktionalitet i programvara för att möjliggöra dokumentation.",
+    a_h4_3: "CoreIT, Systemutvecklare",
+    a_ul_3_li_1:
+      "Utvecklade webbapplikation för kunder att prenumerera på driftinformation om företagstjänster.",
+    a_ul_3_li_2: "Vidareutveckling av applikation för projekthantering.",
+    a_h4_4: "Projekt",
+    a_ul_4_li_1:
+      "Nätverksskanner för att skanna. visualisera nätverk och visa detaljer om anslutna enheter.",
+    a_h4_5: "Projekt",
+    a_ul_5_li_1:
+      "Applikation för att visualisera data och med stöd av ML skapa prognoser. Meddela mig om åtkomst.",
+    b_h2: "Certifieringar",
+    c_h2: "Kompetenser",
+    c_span_1: "Språk",
+    c_span_2: "Ramverk",
+    c_span_3: "Tekniker & Verktyg",
+    c_span_4: "Övriga",
+    other_skills: `
+    Generativ AI,
+    Automation,
+    Dataanalys,
+    Datavisualisering,
+    Datamigrering,
+    Agila metoder,
+    Funktionsanalys,
+    Testning,
+    Teknisk dokumentation,
+    Systemkonfiguration,
+    Cybersäkerhet,
     Nätverksinfrastruktur`,
-    39: "Utbildning",
-    40: "Yrkeshögskoleexamen",
-    41: "Systemutvecklare .NET",
-    42: "Kurser",
-    43: "Nätverkssäkerhet",
-    44: "Nätverksteknik",
-    45: "Nätverksteknologier",
-    46: "Nätverksadministration",
-    47: "Administration av nätverks- och serverutrustning",
-    48: "Övrigt",
-    49: "Referenser lämnas på begäran.",
-    50: "Körkort B",
-    51: "Engelska, professionell nivå.",
-    link: `Visa legitimering <i class="fas fa-external-link-alt"></i>`,
+    d_h2: "Utbildning",
+    d_h4_1: "Yrkeshögskoleexamen",
+    d_p_1: "Systemutvecklare .NET",
+    e_h2: "Övrigt",
+    e_p_1: "Körkort B",
+    e_p_2: "Engelska, flytande.",
   },
   en: {
-    1: "Software Developer | DevOps",
-    2: `Secure infrastructure &bull;
-    Database migration &bull;
-    Generative AI`,
-    3: "Experience",
-    4: "AFRY, Software Developer.",
-    5: "Full time, Aug 2022 - Mar 2023",
-    6: "Web development full stack in ProTAK, a software for production optimization.",
-    7: `C# &bull;
-    .NET &bull;
-    Typescript &bull;
-    React &bull;
-    Knockout &bull;
-    Jquery &bull;
-    Entity Framework &bull;
-    PLSQL &bull;
-    D3 &bull;
-    HTML &bull;
-    SCSS &bull;
-    Azure DevOps &bull;
-    Agile methods`,
-    8: "Flexeurope, Technical Writer.",
-    9: "Full time, Feb 2021 - Jul 2021",
-    10: "Agile work in international team with writing of manuals for web/client software, APIs & integrations.",
-    11: "Manual testing of software and testing of functionality for documentation.",
-    12: "Installations & configuration of software and associated systems, databases, etc.",
-    13: `Technical documentation &bull;
-    Functional analysis &bull;
-    Functional testing &bull;
-    System configuration &bull;
-    Agile methods &bull;
-    Jira`,
-    14: "CoreIT, Software developer.",
-    15: "Full time, Apr 2018 - Jun 2018, Nov 2018 - Apr 2019",
-    16: "Development of web application for customers to subscribe to receive operational information about company services.",
-    17: "Development of windows application for managing projects and business data.",
-    18: "Project, NetViz.",
-    20: "Web based network scanner integrated with Nmap.",
-    21: "Scan and visualize networks and view details about connected devices.",
-    22: `React &bull;
-    Typescript &bull;
-    .NET &bull;
-    C# &bull;
-    Nmap &bull;
-    D3 &bull;
-    Redux &bull;
-    Material UI &bull;
-    Vite &bull;
-    Data visualization &bull;
-    Network technology &bull;
-    Cybersecurity`,
-    23: "Project, Vision.",
-    25: "Data-driven and user-friendly SaaS application built on the Azure platform to visualize data in different ways and create data forecasts with the support of ML. Demo is available at the link below, let me know if you are interested.",
-    26: `Open in new tab <i class="fas fa-external-link-alt"></i>`,
-    27: "Licenses and certificates",
-    28: "Issued feb. 2024",
-    29: "Issued feb. 2024",
-    30: "Issued jan. 2024",
-    31: "Issued jan. 2024",
-    32: "Issued jan. 2024",
-    33: "Issued dec. 2023",
-    34: "Skills",
-    35: "Languages & frameworks",
-    36: "Technologies",
-    37: "Other",
-    38: `Web development &bull;
-    Frontend &bull;
-    Backend &bull;
-    Software development &bull;
-    Generative AI &bull;
-    Automation &bull;
-    Data analysis &bull;
-    Data visualization &bull;
-    Identity and Access Management (IAM) &bull;
-    Database migration &bull;
-    Agile methods &bull;
-    Functional analysis &bull;
-    Functional testing &bull;
-    Unit testing &bull;
-    Technical documentation &bull;
-    System configuration &bull;
-    Cybersecurity &bull;
+    loc: `<i class="fas fa-map-marker-alt"></i> Ornskoldsvik, Sweden`,
+    a_h2: "Experience",
+    a_h4_1: "AFRY, Software Developer.",
+    a_ul_1_li_1:
+      "Web development in ProTAK, a software for production optimization.",
+    a_ul_1_li_2:
+      "Developed BI report solution for client to enable simpler reporting of production data.",
+    a_ul_1_li_3:
+      "Work with modernization of components from KnockoutJS to ReactJS and various support matters.",
+    a_h4_2: "Flexeurope, Technical Writer",
+    a_ul_2_li_1:
+      "Completed work on writing software manuals for older and up to current versions.",
+    a_ul_2_li_2:
+      "System configuration and testing of software functionality to enable documentation.",
+    a_h4_3: "CoreIT, Software Developer",
+    a_ul_3_li_1:
+      "Developed web application for customers to subscribe to recieve operational information for business services.",
+    a_ul_3_li_2: "Development of application for managing projects.",
+    a_h4_4: "Project",
+    a_ul_4_li_1:
+      "Network scanner for scanning, visualizing networks and viewing details about connected devices.",
+    a_h4_5: "Project",
+    a_ul_5_li_1:
+      "Application for visualizing data and with support of ML create forecasts. Notify me about access.",
+    b_h2: "Certifications",
+    c_h2: "Skills",
+    c_span_1: "Languages",
+    c_span_2: "Frameworks",
+    c_span_3: "Technologies & Tools",
+    c_span_4: "Other",
+    other_skills: `
+    Generative AI,
+    Automation,
+    Data analysis,
+    Data visualization,
+    Data migration,
+    Agile methods,
+    Functional analysis,
+    Testing,
+    Technical writing,
+    System configuration,
+    Cybersecurity,
     Network infrastructure`,
-    39: "Education",
-    40: "HVE Diploma",
-    41: "Software Developer .NET",
-    42: "Courses",
-    43: "Network security",
-    44: "Network technology",
-    45: "Network technologies",
-    46: "Network administration",
-    47: "Administration of network and server equipment",
-    48: "Other",
-    49: "References available upon request.",
-    50: "Drivers license",
-    51: "Swedish, native.",
-    link: `Show identification <i class="fas fa-external-link-alt"></i>`,
+    d_h2: "Education",
+    d_h4_1: "HVE Diploma",
+    d_p_1: "Software Developer .NET",
+    e_h2: "Other",
+    e_p_1: "Drivers license",
+    e_p_2: "Swedish, native.",
   },
 };
 
